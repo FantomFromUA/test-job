a350df603390cc5b342131301c18051fab3ada76
Erik <erik.toroni@gmail.com>
2023-09-27 19:49:32 +0300

    feat: project initialization

---
+++ src/test/java/com/clearsolution/testjob/TestJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clearsolution.testjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
